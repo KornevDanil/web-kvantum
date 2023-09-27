023cd3149277a9ecbd4775aea74bba4d30e49b81
KornevDanil <Danilka040209@gmail.com>
2023-09-27 15:48:21 +0700

    take ex4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Задание - 1
 console.log('Задание-1')
-console.log('Существует достаточно много экранирующих последовательностей.\n Например последовательность"\\n"делает перевод на новую строку!')
+console.log("Существует достаточно много экранирующих последовательностей.\n Например последовательность\"\\n\"делает перевод на новую строку!")
 // Задание - 2 
 console.log('Задание-2')
 let a = 4
@@ -11,7 +11,17 @@ console.log(result)
 // Задание - 3
 console.log('Задание-3')
 let text1 = 'Привет,мир'
-console.log(text1 )
+console.log(text1.toUpperCase())
 // Задание - 4
 console.log('Задание-4')
+let text = prompt()
+alert(text)
+let value = text
+if (value == 0) {
+    console.log('Это ноль')
+} else if (value <0) {
+    console.log('Число отрицательное')
+} else if (value >0) {
+    console.log('Число положительое')
+}
 
