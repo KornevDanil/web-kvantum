09d4d1094f4ab1693e125777abb32272c94d2344
KornevDanil <Danilka040209@gmail.com>
2023-10-02 14:58:04 +0700

    делаю

---
+++ script.js
@@ -0,0 +1,18 @@
+// Задание - 1
+console.log('Задание-1')
+let text = prompt()
+alert(text)
+let number = text
+
+if (number == 0) {
+    console.log('Это ноль')
+} else if (number <0) {
+    console.log('Число отрицательное')
+} else if (number >0) {
+    console.log('Число положительое')
+}
+
+// Задание - 2 
+console.log('Задание-2')
+
+
